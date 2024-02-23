5b14c618930385fe5d060929ab6a27485301eb72
neeyatl <neeyat.lotlikar@gmail.com>
2024-02-23 16:21:38 +0530

    tried query parameters

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,43 @@
-from .schema import Category
 from fastapi.routing import APIRouter
 
-router = APIRouter(prefix='/api/v1')
+from .schema import Category, mock_db_data
+
+router = APIRouter(prefix="/api/v1")
+
 
 @router.get("/")
 async def root():
     return {"message": "Hello World"}
 
+
+@router.get("/items/")
+async def read_item(skip: int = 0, limit: int = 5):
+    return mock_db_data[skip : skip + limit]
+
+
 @router.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
+async def read_item(
+    item_id: int, vendor: str, q: str | None = None, short: bool = True
+):
+    data = (
+        mock_db_data[item_id]
+        if item_id < len(mock_db_data)
+        else {"message": "Item not found"}
+    )
+    item = {"item_id": item_id, "vendor": vendor, **data}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {
+                "description": (
+                    "This is an amazing item that has a long description "
+                    f"that spans multiple lines as it is {item['description']}."
+                )
+            }
+        )
+    return item
+
 
 @router.get("/items/{category}/{item_id}")
 async def read_item(category: Category, item_id: int):
@@ -23,7 +51,7 @@ async def read_item(category: Category, item_id: int):
                     "consume food for energy, and reproduce. They can "
                     "be found in various habitats and exhibit a wide "
                     "range of behaviors and characteristics."
-                )
+                ),
             }
         case Category.vehicles:
             return {
@@ -34,7 +62,7 @@ async def read_item(category: Category, item_id: int):
                     "forms, such as cars, trucks, airplanes, bicycles, "
                     "and boats, and are powered by various sources, "
                     "including gasoline, electricity, and human energy."
-                )
+                ),
             }
         case Category.foods:
             return {
@@ -46,10 +74,7 @@ async def read_item(category: Category, item_id: int):
                     "their nutritional content, such as fruits, "
                     "vegetables, grains, proteins, and dairy products. "
                     "They are essential for maintaining health and well-being."
-                )
+                ),
             }
         case _:
-            return {
-                **common_fields,
-                "message": "Unknown Category"
-            }
+            return {**common_fields, "message": "Unknown Category"}
--- app/schema.py
+++ app/schema.py
@@ -1,8 +1,20 @@
 from enum import Enum
 
+mock_db_data = [
+    {"name": "Apple", "type": "Fruit", "description": "Delicious"},
+    {"name": "Car", "type": "Vehicle", "description": "Vroom"},
+    {"name": "Book", "type": "Book", "description": "Informative"},
+    {"name": "Sunglasses", "type": "Accessories", "description": "Stylish"},
+    {"name": "Laptop", "type": "Portable", "description": "Powerful"},
+    {"name": "Bicycle", "type": "Vehicle", "description": "Eco-friendly"},
+    {"name": "Watch", "type": "Accessories", "description": "Elegant"},
+    {"name": "Headphones", "type": "Accessories", "description": "Wireless"},
+    {"name": "Backpack", "type": "Luggage", "description": "Spacious"},
+    {"name": "Camera", "type": "Electronics", "description": "High-resolution"},
+]
+
 
 class Category(str, Enum):
     animals = "animals"
     vehicles = "vehicles"
     foods = "foods"
-
